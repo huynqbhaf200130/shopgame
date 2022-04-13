74f4936fb8c2210857439da34841200ad40074c0
Tuitenlamanh <manhvannguyen1998@gmail.com>
2022-04-13 16:01:23 +0700

    sua product table

--- database/migrations/2022_04_13_082104_create_product_table.php
+++ database/migrations/2022_04_13_082104_create_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('product', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('product_name',100);
+            $table->float('product_price');
+            $table->integer('quantity');
+            $table->string('product_des',200);
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category');
         });
     }
 
