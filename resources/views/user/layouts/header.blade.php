5c9462eda03f890b0e20defade6ef92495b2bd6b
huynqbhaf200130 <huynqbhaf200130@fpt.edu.vn>
2022-04-13 17:45:44 +0700

    sua logo

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -32,10 +32,8 @@
             <div class="row">
                 <div class="col-lg-2">
                     <div class="header__logo">
-                        <a href="./index.html">
-                        <video>
-                            <source src="asset{{'video/Shopgame.mp4'}}">
-                        </video>    
+                        <a href="">
+                            <img src="{{asset('img/logo.png')}}" alt="">
                         </a>
                     </div>
                 </div>
