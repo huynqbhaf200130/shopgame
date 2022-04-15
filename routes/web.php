c20239e5c8ee60ce5707db3122df70431c003d6e
huynqbhaf200130 <huynqbhaf200130@fpt.edu.vn>
2022-04-15 17:51:50 +0700

    sua env

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 
 Route::group(['prefix'=>'user'], function(){
